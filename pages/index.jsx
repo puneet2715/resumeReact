5c3514a1c49d6708114221e4b5867d7c52724312
Puneet Arora <puneet2715@gmail.com>
2023-05-10 03:21:57 +0530

    resume link updated

--- pages/index.jsx
+++ pages/index.jsx
@@ -34,7 +34,7 @@ export default function Home() {
               <li>
                 <a
                   className="px-4 py-2 border-none rounded-md ml-text-white bg-gradient-to-r from-cyan-500 text- to-teal-500 max-xs:text-sm"
-                  href="https://filebrowser.shadowdragon.one/share/Pug99a66"
+                  href="https://filebrowser.shadowdragon.one/files/Puneet%20Arora%20(1).pdf"
                   target={"_blank"}
                   rel="noreferrer"
                 >
