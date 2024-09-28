2c894e80087f094f0c88a15f8f77bfbfd81a2643
Puneet Arora <puneet2715@gmail.com>
2024-09-28 17:18:14 +0530

    quite a few fixes for the UX and speedinsights package addded

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -8,7 +8,20 @@ export default function Document() {
           name="description"
           content="Portfolio/Resume Site of Puneet Arora"
         />
-        <link rel="icon" href="/favicon.ico" />
+        {/* Facebook Meta Tags */}
+        <meta property="og:title" content="Resume/Portfolio" />
+        <meta property="og:description" content="Portfolio/Resume Site of Puneet Arora" />
+        <meta property="og:image" content="https://ik.imagekit.io/puneet2715/icon.jpg?tr=w-180,h-180" />
+        <meta property="og:url" content="https://www.resume.shadowdragon.dev" />
+        <meta property="og:type" content="website" />
+
+        {/* Twitter Meta Tags */}
+        <meta name="twitter:card" content="I solve complex problems with a outcomes-centered approach. See how at resume.shadowdragon.dev" />
+        <meta property="twitter:domain" content="resume.shadowdragon.dev" />
+        <meta property="twitter:url" content="https://www.resume.shadowdragon.dev" />
+        <meta name="twitter:title" content="Resume/Portfolio" />
+        <meta name="twitter:description" content="Portfolio/Resume Site of Puneet Arora" />
+        <meta name="twitter:image" content="https://ik.imagekit.io/puneet2715/icon.jpg?tr=w-180,h-180" />
       </Head>
       <body>
         <Main />
--- pages/index.jsx
+++ pages/index.jsx
@@ -14,13 +14,16 @@ export default function Home() {
     <div>
       <Head>
         <title>Resume/Portfolio</title>
+        <link rel="apple-touch-icon" href="https://ik.imagekit.io/puneet2715/icon.jpg?tr=w-180,h-180" />
+        <link rel="icon" type="image/jpg" sizes="32x32" href="https://ik.imagekit.io/puneet2715/icon.jpg?tr=w-32,h-32" />
+        <link rel="icon" type="image/jpg" sizes="16x16" href="https://ik.imagekit.io/puneet2715/icon.jpg?tr=w-16,h-16" />
       </Head>
       <main className="px-10 bg-gray-900 md:px-20 lg:px-40 max-xs:px-4 max-xxs:px-4">
-        <section className="min-h-screen">
+        <section className="min-h-screen max-w-6xl mx-auto">
           <nav className="flex justify-between py-8 my-0 mb-12 text-white max-xs:block">
-            <h1 className="text-xl font-burtons max-xs:text-sm max-xs:flex max-xs:justify-center">
+            {/* <h1 className="text-xl font-burtons max-xs:text-sm max-xs:flex max-xs:justify-center">
               developedbyPuneet
-            </h1>
+            </h1> */}
             <ul className="flex justify-center max-xs:mt-6">
               <li>
                 <a
@@ -86,7 +89,7 @@ export default function Home() {
             </div>
           </div>
         </section>
-        <section>
+        <section className="max-w-6xl mx-auto">
           <div className="mx-auto">
             <h2 className="py-1 text-3xl text-white ">Education</h2>
             <h3 className="py-1 text-2xl text-teal-400 ">
@@ -103,149 +106,143 @@ export default function Home() {
               <span className="text-neutral-400">2019-2022 </span>
             </p>
           </div>
-          <div className="pt-10">
-            <h2 className="py-1 text-3xl text-white ">Experience</h2>
-          </div>
-          <div className="gap-10 lg:flex max-w-8xl max-h-max">
-            <div className="flex-1 p-10 my-10 text-center shadow-lg rounded-xl bg-gradient-to-r from-cyan-500 text- to-teal-600">
-              <Image
-                lazy="true"
-                src={"https://ik.imagekit.io/puneet2715/3d-comp.jpg"}
-                width={300}
-                height={250}
-                alt="random png"
-              />
-              <p className="text-xs">
-                Illustration from{" "}
-                <a
-                  href="https://www.freepik.com/free-psd/3d-rendering-graphic-design_31164759.htm#&position=3&from_view=undefinedhttps://www.freepik.com/free-psd/3d-rendering-graphic-design_31164759.htm#&position=3&from_view=undefined"
-                  target={"_blank"}
-                  rel="noreferrer nopener nofollow"
-                  className="underline"
-                >
-                  Freepik
-                </a>
-              </p>
-              <h3 className="pt-8 pb-2 text-lg font-medium text-purple-600">
-                Back-End Dev @ <a
-                  href="https://www.linkedin.com/company/attendit1/about/"
-                  target={"_blank"}
-                  rel="noreferrer nopener nofollow"
-                  className="hover:underline"
-                >
-                  ATTENDIT
-                </a>
-              </h3>
-              <h4 className="-mt-3 font-medium text-purple-600">
-                April 2020 - June 2020  
-              </h4>
-              <p className="py-2">
-                Comepleted day to day tasks like -&gt; <br /> Contribution in
-                development of the REST APIs and password hashing and storing.
-              </p>
-              <h4 className="py-4 text-purple-600">Tech Stack </h4>
-              <p className="py-1 text-gray-900">Nest.Js - Javascript</p>
-              <p className="py-1 text-gray-900">MongoDb - Database (NoSQL)</p>
-              <p className="py-1 text-gray-900">VCS -&gt; Git/Github</p>
-            </div>
-            <div className="flex-1 p-10 my-10 text-center shadow-lg rounded-xl bg-gradient-to-r from-cyan-500 text- to-teal-600">
-              <Image
-                lazy="true"
-                src={"https://ik.imagekit.io/puneet2715/3d-comp.jpg"}
-                width={300}
-                height={250}
-                alt="random png"
-              />
-              <p className="text-xs">
-                Illustration from{" "}
-                <a
-                  href="https://www.freepik.com/free-psd/3d-rendering-graphic-design_31164759.htm#&position=3&from_view=undefinedhttps://www.freepik.com/free-psd/3d-rendering-graphic-design_31164759.htm#&position=3&from_view=undefined"
-                  target={"_blank"}
-                  rel="noreferrer nopener nofollow"
-                  className="underline"
-                >
-                  Freepik
-                </a>
-              </p>
-              <h3 className="pt-8 pb-2 text-lg font-medium text-purple-600">
-                Software Developer @{" "}
-                <a
-                  href="https://www.easyops.in/"
-                  target={"_blank"}
-                  rel="noreferrer nopener nofollow"
-                  className="hover:underline"
-                >
-                  EasyOps
-                </a>
-              </h3>
-              <h4 className="-mt-3 font-medium text-purple-600">
-                Feb 2023 - April 2023  
-              </h4>
-              <p className="py-2">
-                Development Style -&gt; AGILE <br />
-              </p>
-              {/* <p className="py-2">
-                Comepleted assignments and daily standups <br />
-              </p> */}
-              <h4 className="py-4 text-purple-600">Tech Stack </h4>
-              <p className="py-1 text-gray-900">
-                Next.Js/React.Js - Javascript/Typescript
-              </p>
-              <p className="py-1 text-gray-900">
-                MaterialUi/TailwindCss/ChakraUi - Css
-              </p>
-              <p className="py-1 text-gray-900">MongoDb - Database (NoSQL)</p>
-              <p className="py-1 text-gray-900">Jest, ts-jest, React Testing Library (Testing)</p>
-              <p className="py-1 text-gray-900">VCS -&gt; Git/Github</p>
-            </div>
-
-            <div className="flex-1 p-10 my-10 text-center shadow-lg rounded-xl bg-gradient-to-r from-cyan-500 text- to-teal-600">
-              <Image
-                lazy="true"
-                src={"https://ik.imagekit.io/puneet2715/open-source.jpg"}
-                width={300}
-                height={250}
-                alt="random png"
-              />
-              <p className="text-xs">
-                Illustration from{" "}
-                <a
-                  href="https://www.freepik.com/free-photo/3d-illustration-open-closed-books-pencil_33309550.htm#&position=1&from_view=undefined"
-                  target={"_blank"}
-                  rel="noreferrer nopener nofollow"
-                  className="underline"
-                >
-                  Freepik
-                </a>
-              </p>
-              <h3 className="pt-8 pb-2 text-lg font-medium text-purple-600">
-                Associate Software Engineer @{" "}
-                <a
-                  href="https://www.quantela.com/"
-                  target={"_blank"}
-                  rel="noreferrer nopener nofollow"
-                  className="hover:underline"
-                >
-                  Quantela
-                </a>
-              </h3>
-              <h4 className="-mt-3 font-medium text-purple-600">
-                <time>June 2023 - Present</time>  
-              </h4>
-              <p className="py-2">
-                • Worked on Upgrading a Platform ( Angular )
-                <br />
-                &bull; Module Development - UI / Unit Testing / Providing Critical Feedback on UX ( Angular )
-                <br />
-                • Helping the team as much as I can {/* press numlock then hold alt and then press 0 1 4 9 in succession to get the bullet point or &bull */}  
-              </p>
-              <h4 className="py-4 text-purple-600">Tech Stack </h4>
-              <p className="py-1 text-gray-900">JavaScript - Angular/NodeJs</p>
-              <p className="py-1 text-gray-900">VCS -&gt; Git/Github</p>
-            </div>
-          </div>
         </section>
-        <section className="py-10">
+        <section className="max-w-6xl mx-auto">
+        <div className="mx-auto">
+          <h2 className="py-1 text-3xl text-white">Experience</h2>
+        </div>
+          <div className="gap-8 lg:flex max-w-6xl max-h-fit mx-auto">
+          <div className="flex-1 p-6 my-4 text-center shadow-lg rounded-xl bg-gradient-to-r from-cyan-700 to-teal-700">
+            <Image
+              lazy="true"
+              src={"https://ik.imagekit.io/puneet2715/open-source.jpg"}
+              width={200}
+              height={150}
+              alt="Open source illustration"
+            />
+            <p className="text-xs text-gray-300 mt-2">
+              Illustration from{" "}
+              <a
+                href="https://www.freepik.com"
+                target="_blank"
+                rel="noreferrer nopener nofollow"
+                className="underline"
+              >
+                Freepik
+              </a>
+            </p>
+            <h3 className="pt-4 pb-2 text-lg font-medium text-teal-300">
+              Associate Software Engineer @{" "}
+              <a
+                href="https://www.quantela.com/"
+                target="_blank"
+                rel="noreferrer nopener nofollow"
+                className="hover:underline"
+              >
+                Quantela
+              </a>
+            </h3>
+            <h4 className="font-medium text-cyan-300">
+              June 2023 - Present
+            </h4>
+            <ul className="text-left text-gray-200 list-disc list-inside py-4 leading-relaxed">
+              <li>Worked on Upgrading a Platform (Angular)</li>
+              <li>Module Development - UI / Unit Testing / Providing Critical Feedback on UX (Angular)</li>
+              <li>Helping the team as much as I can</li>
+            </ul>
+            <h4 className="py-2 text-teal-300">Tech Stack</h4>
+            <p className="py-1 text-gray-200">JavaScript - Angular/NodeJs</p>
+            <p className="py-1 text-gray-200">VCS - Git/Github</p>
+            </div>
+            
+            <div className="flex-1 p-6 my-4 text-center shadow-lg rounded-xl bg-gradient-to-r from-cyan-700 to-teal-700">
+            <Image
+              lazy="true"
+              src={"https://ik.imagekit.io/puneet2715/3d-comp.jpg"}
+              width={200}
+              height={150}
+              alt="Computer illustration"
+            />
+            <p className="text-xs text-gray-300 mt-2">
+              Illustration from{" "}
+              <a
+                href="https://www.freepik.com/free-psd/3d-rendering-graphic-design_31164759.htm#&position=3&from_view=undefined"
+                target={"_blank"}
+                rel="noreferrer nopener nofollow"
+                className="underline"
+              >
+                Freepik
+              </a>
+            </p>
+            <h3 className="pt-4 pb-2 text-lg font-medium text-teal-300">
+              Software Developer @{" "}
+              <a
+                href="https://www.easyops.in/"
+                target={"_blank"}
+                rel="noreferrer nopener nofollow"
+                className="hover:underline"
+              >
+                EasyOps
+              </a>
+            </h3>
+            <h4 className="font-medium text-cyan-300">
+              Feb 2023 - April 2023  
+            </h4>
+            <p className="py-2 text-gray-200">
+              Development Style -&gt; AGILE
+            </p>
+            <h4 className="py-2 text-teal-300">Tech Stack</h4>
+            <p className="py-1 text-gray-200">Next.Js/React.Js - Javascript/Typescript</p>
+            <p className="py-1 text-gray-200">MaterialUi/TailwindCss/ChakraUi - Css</p>
+            <p className="py-1 text-gray-200">MongoDb - Database (NoSQL)</p>
+            <p className="py-1 text-gray-200">Jest, ts-jest, React Testing Library (Testing)</p>
+            <p className="py-1 text-gray-200">VCS -&gt; Git/Github</p>
+          </div>
+            
+          <div className="flex-1 p-6 my-4 text-center shadow-lg rounded-xl bg-gradient-to-r from-cyan-700 to-teal-700">
+            <Image
+              lazy="true"
+              src={"https://ik.imagekit.io/puneet2715/3d-comp.jpg"}
+              width={200}
+              height={150}
+              alt="Computer illustration"
+            />
+            <p className="text-xs text-gray-300 mt-2">
+              Illustration from{" "}
+              <a
+                href="https://www.freepik.com/free-psd/3d-rendering-graphic-design_31164759.htm#&position=3&from_view=undefined"
+                target={"_blank"}
+                rel="noreferrer nopener nofollow"
+                className="underline"
+              >
+                Freepik
+              </a>
+            </p>
+            <h3 className="pt-4 pb-2 text-lg font-medium text-teal-300">
+              Back-End Dev @ <a
+                href="https://www.linkedin.com/company/attendit1/about/"
+                target={"_blank"}
+                rel="noreferrer nopener nofollow"
+                className="hover:underline"
+              >
+                ATTENDIT
+              </a>
+            </h3>
+            <h4 className="font-medium text-cyan-300">
+              April 2020 - June 2020  
+            </h4>
+            <p className="py-2 text-gray-200">
+              Contribution in development of REST APIs and password hashing/storing.
+            </p>
+            <h4 className="py-2 text-teal-300">Tech Stack</h4>
+            <p className="py-1 text-gray-200">Nest.Js - Javascript</p>
+            <p className="py-1 text-gray-200">MongoDb - Database (NoSQL)</p>
+            <p className="py-1 text-gray-200">VCS - Git/Github</p>
+          </div>
+          
+        </div>
+        </section>
+        <section className="py-10 max-w-6xl mx-auto">
           <div>
             <h3 className="py-1 text-3xl text-white">Portfolio</h3>
           </div>
@@ -275,7 +272,7 @@ export default function Home() {
                 </a>
               </div>
             </div>
-            <div className="flex-1 basis-1/3 ">
+            {/* <div className="flex-1 basis-1/3 ">
               <iframe
                 lazy="true"
                 layout="responsive"
@@ -292,7 +289,7 @@ export default function Home() {
                   <h3 className="py-1 text-3xl text-white">Live</h3>
                 </a>
               </div>
-            </div>
+            </div> */}
             <div className="flex-1 basis-1/3">
               <iframe
                 lazy="true"
