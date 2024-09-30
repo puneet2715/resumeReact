e814f2c78836a329451e304190dac1be45da82b0
Puneet <puneet2715@gmail.com>
2024-09-30 13:18:11 +0530

    twitter url fix

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -22,7 +22,7 @@ export default function Document() {
         <meta name="twitter:creator" content="@puneet2715" />
         <meta name="twitter:summary" content="I solve complex problems with a outcomes-centered approach. See how at resume.shadowdragon.dev" />
         <meta property="twitter:domain" content="resume.shadowdragon.dev" />
-        <meta property="twitter:url" content="https://www.resume.shadowdragon.dev" />
+        <meta property="twitter:url" content="https://resume.shadowdragon.dev" />
         <meta name="twitter:title" content="Resume/Portfolio" />
         <meta name="twitter:description" content="Portfolio/Resume Site of Puneet Arora" />
         <meta name="twitter:image" content="https://ogcdn.net/e4b8c678-7bd5-445d-ba03-bfaad510c686/v4/resume.shadowdragon.dev/Resume%2FPortfolio/https%3A%2F%2Fik.imagekit.io%2Fpuneet2715%2Ficon.jpg%3Ftr%3Dw-180%2Ch-180/og.png" />
